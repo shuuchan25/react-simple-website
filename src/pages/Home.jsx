336bf22fae4b71dbd40214ed67a4054eb7317734
ahnafts <ahnaftsaqif25@gmail.com>
2024-07-31 15:07:17 +0700

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,56 @@
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import Tutors from "../components/Tutors";
+import Partners from "../components/Partners";
+import Contact from "../components/Contact";
+
+import { homeSection } from "../data/HomeSection";
+import { coursesSection } from "../data/CoursesSection";
+import { tutorsSection, tutorsList } from "../data/TutorsSection";
+import { partnersSection, partnersList } from "../data/PartnersSection";
+import { contactSection } from "../data/ContactSection";
+
+import "../styles/Home.css";
+import parse from "html-react-parser";
+
+function Home() {
+  return (
+    <>
+      <Navbar />
+
+      <div className="wrapper">
+        {/* <!-- untuk home --> */}
+        <section id="home">
+          <img src={homeSection.image} />
+          <div className="kolom">{parse(homeSection.content)}</div>
+        </section>
+
+        {/* <!-- untuk courses --> */}
+        <section id="courses">
+          <div className="kolom">{parse(coursesSection.content)}</div>
+          <img src={coursesSection.image} />
+        </section>
+
+        {/* Tutors */}
+        <section id="tutors">
+          <div className="tengah">
+            <div className="kolom">{parse(tutorsSection.content)}</div>
+            <Tutors tutorsList={tutorsList} />
+          </div>
+        </section>
+
+        <section id="partners">
+          <div className="tengah">
+            <div className="kolom">{parse(partnersSection.content)}</div>
+            <Partners partnersList={partnersList} />
+          </div>
+        </section>
+      </div>
+
+      <Contact contactSection={contactSection} />
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
